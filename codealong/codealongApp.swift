e405b52b08d07b781a6d32ffbfa8a8cd48ca631c
Lily Jimenez <carlosaurus38@gmail.com>
2025-09-26 12:22:48 -0700

    Initial Commit

---
+++ codealong/codealongApp.swift
@@ -0,0 +1,17 @@
+//
+//  codealongApp.swift
+//  codealong
+//
+//  Created by Jimenez, Carlos - Student on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct codealongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ codealongTests/codealongTests.swift
@@ -0,0 +1,17 @@
+//
+//  codealongTests.swift
+//  codealongTests
+//
+//  Created by Jimenez, Carlos - Student on 9/26/25.
+//
+
+import Testing
+@testable import codealong
+
+struct codealongTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
